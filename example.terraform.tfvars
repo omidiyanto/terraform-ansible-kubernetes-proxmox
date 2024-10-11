34eb299c8e73d480b3a6f820f1d142859fc39919
O. Midiyanto <omidiyanto7@gmail.com>
2024-10-11 13:39:08 +0700

    first init

---
+++ example.terraform.tfvars
@@ -0,0 +1,10 @@
+# API proxmox
+proxmox_api_url ="https://PROXMOX_SERVER:8006/api2/json/"
+proxmox_api_token_id = "PROXMOX_API_TOKEN_ID"
+proxmox_api_token_secret = "PROXMOX_API_TOKEN_SECRET"
+
+# cloud-init configuration
+ci_user = "YOUR_CLOUD_INIT_USER"
+ci_password = "YOUR_CLOUD_INIT_USER_PASSWORD"
+ci_ssh_public_key = "~/.ssh/id_rsa.pub"
+ci_ssh_private_key = "~/.ssh/id_rsa"
---
+++ main.tf
@@ -0,0 +1,121 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.1-rc3"
+    }
+  }
+}
+
+resource "proxmox_vm_qemu" "k8s-master" {
+    name = "k8s-master"
+    target_node = "proxmox"
+    vmid = 300
+
+    # The template name to clone this vm from
+    clone = "ubuntu-template"
+    full_clone = true
+
+    ciuser = var.ci_user
+    cipassword = var.ci_password
+    sshkeys = file(var.ci_ssh_public_key)
+
+    # Activate QEMU agent for this VM
+    agent = 1
+    cores = 2
+    memory = 2048
+    os_type = "cloud-init"
+    bootdisk = "scsi0"
+    scsihw = "virtio-scsi-pci"
+
+    # Setup the disk
+    disks {
+        ide {
+            ide0 {
+                cloudinit {
+                    storage = "local"
+                }
+            }
+        }
+        scsi {
+            scsi0 {
+                disk {
+                    size    = 10
+                    storage = "local"
+                }
+            }
+        }
+    }
+
+    # Setup the network interface
+    network {
+        model = "virtio"
+        bridge = "vmbr0"
+    }
+
+    boot = "order=scsi0"
+    ipconfig0 = "ip=dhcp"
+    
+    lifecycle {
+    ignore_changes = [ 
+      network
+     ]
+  }
+}
+
+resource "proxmox_vm_qemu" "k8s-workers" {
+    count = var.count_number
+    name = "k8s-worker${count.index + 1}"
+    target_node = "proxmox"
+    vmid = 300 + (count.index + 1)
+
+    # The template name to clone this vm from
+    clone = "ubuntu-template"
+    full_clone = true
+
+    ciuser = var.ci_user
+    cipassword = var.ci_password
+    sshkeys = file(var.ci_ssh_public_key)
+
+    # Activate QEMU agent for this VM
+    agent = 1
+    cores = 2
+    memory = 2048
+    os_type = "cloud-init"
+    bootdisk = "scsi0"
+    scsihw = "virtio-scsi-pci"
+
+    # Setup the disk
+    disks {
+        ide {
+            ide0 {
+                cloudinit {
+                    storage = "local"
+                }
+            }
+        }
+        scsi {
+            scsi0 {
+                disk {
+                    size    = 10
+                    storage = "local"
+                }
+            }
+        }
+    }
+
+    # Setup the network interface
+    network {
+        model = "virtio"
+        bridge = "vmbr0"
+    }
+
+    boot = "order=scsi0"
+    ipconfig0 = "ip=dhcp"
+    
+    lifecycle {
+    ignore_changes = [ 
+      network
+     ]
+  }
+}
