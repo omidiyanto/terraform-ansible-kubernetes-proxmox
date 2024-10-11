ec46a847e9e99ca2d0204b5a4a5e11974aa0b070
O. Midiyanto <omidiyanto7@gmail.com>
2024-10-11 14:06:13 +0700

    feature: added crate ansible-inventory

--- main.tf
+++ main.tf
@@ -8,126 +8,136 @@ terraform {
 }
 
 resource "proxmox_vm_qemu" "k8s-master" {
-    name = "k8s-master"
-    target_node = "proxmox"
-    vmid = 300
+  name        = "k8s-master"
+  target_node = "proxmox"
+  vmid       = 300
+  clone      = "ubuntu-template"
+  full_clone = true
 
-    # The template name to clone this vm from
-    clone = "ubuntu-template"
-    full_clone = true
+  ciuser    = var.ci_user
+  cipassword = var.ci_password
+  sshkeys   = file(var.ci_ssh_public_key)
 
-    ciuser = var.ci_user
-    cipassword = var.ci_password
-    sshkeys = file(var.ci_ssh_public_key)
+  agent     = 1
+  cores     = 2
+  memory    = 2048
+  os_type   = "cloud-init"
+  bootdisk  = "scsi0"
+  scsihw    = "virtio-scsi-pci"
 
-    # Activate QEMU agent for this VM
-    agent = 1
-    cores = 2
-    memory = 2048
-    os_type = "cloud-init"
-    bootdisk = "scsi0"
-    scsihw = "virtio-scsi-pci"
-
-    # Setup the disk
-    disks {
-        ide {
-            ide0 {
-                cloudinit {
-                    storage = "local"
-                }
-            }
+  disks {
+    ide {
+      ide0 {
+        cloudinit {
+          storage = "local"
         }
-        scsi {
-            scsi0 {
-                disk {
-                    size    = 10
-                    storage = "local"
-                }
-            }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size    = 10
+          storage = "local"
         }
+      }
     }
+  }
 
-    # Setup the network interface
-    network {
-        model = "virtio"
-        bridge = "vmbr0"
-    }
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
 
-    boot = "order=scsi0"
-    ipconfig0 = "ip=dhcp"
-    
-    lifecycle {
+  boot     = "order=scsi0"
+  ipconfig0 = "ip=dhcp"
+  
+  lifecycle {
     ignore_changes = [ 
       network
-     ]
+    ]
   }
 }
 
 resource "proxmox_vm_qemu" "k8s-workers" {
-    count = var.vm_count
-    name = "k8s-worker-${count.index + 1}"
-    target_node = "proxmox"
-    vmid = 300 + (count.index + 1)
+  count       = var.vm_count
+  name        = "k8s-worker-${count.index + 1}"
+  target_node = "proxmox"
+  vmid        = 301 + count.index
+  clone       = "ubuntu-template"
+  full_clone  = true
 
-    # The template name to clone this vm from
-    clone = "ubuntu-template"
-    full_clone = true
+  ciuser    = var.ci_user
+  cipassword = var.ci_password
+  sshkeys   = file(var.ci_ssh_public_key)
 
-    ciuser = var.ci_user
-    cipassword = var.ci_password
-    sshkeys = file(var.ci_ssh_public_key)
+  agent     = 1
+  cores     = 2
+  memory    = 2048
+  os_type   = "cloud-init"
+  bootdisk  = "scsi0"
+  scsihw    = "virtio-scsi-pci"
 
-    # Activate QEMU agent for this VM
-    agent = 1
-    cores = 2
-    memory = 2048
-    os_type = "cloud-init"
-    bootdisk = "scsi0"
-    scsihw = "virtio-scsi-pci"
-
-    # Setup the disk
-    disks {
-        ide {
-            ide0 {
-                cloudinit {
-                    storage = "local"
-                }
-            }
+  disks {
+    ide {
+      ide0 {
+        cloudinit {
+          storage = "local"
         }
-        scsi {
-            scsi0 {
-                disk {
-                    size    = 10
-                    storage = "local"
-                }
-            }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size    = 10
+          storage = "local"
         }
+      }
     }
+  }
 
-    # Setup the network interface
-    network {
-        model = "virtio"
-        bridge = "vmbr0"
-    }
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
 
-    boot = "order=scsi0"
-    ipconfig0 = "ip=dhcp"
-    
-    lifecycle {
+  boot     = "order=scsi0"
+  ipconfig0 = "ip=dhcp"
+  
+  lifecycle {
     ignore_changes = [ 
       network
-     ]
+    ]
   }
 }
 
-output "vm-info" {
-  value = [
-    for vm in concat(
-      [proxmox_vm_qemu.k8s-master],
-      proxmox_vm_qemu.k8s-workers
-    ) : {
-      hostname = vm.name
-      ip_addr  = vm.default_ipv4_address
-    }
+output "vm_info" {
+  value = {
+    master = {
+      hostname = proxmox_vm_qemu.k8s-master.name
+      ip_addr  = proxmox_vm_qemu.k8s-master.default_ipv4_address
+    },
+    workers = [
+      for vm in proxmox_vm_qemu.k8s-workers : {
+        hostname = vm.name
+        ip_addr  = vm.default_ipv4_address
+      }
+    ]
+  }
+}
+
+resource "local_file" "create_ansible_inventory" {
+  depends_on = [
+    proxmox_vm_qemu.k8s-master,
+    proxmox_vm_qemu.k8s-workers
   ]
+
+  content = <<EOT
+[master-node]
+${proxmox_vm_qemu.k8s-master.default_ipv4_address}
+
+[worker-node]
+${join("\n", [for worker in proxmox_vm_qemu.k8s-workers : worker.default_ipv4_address])}
+EOT
+
+  filename = "${path.module}/inventory.ini"
 }
